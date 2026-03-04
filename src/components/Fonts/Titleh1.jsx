80b90e1a27d8e6403610074c4ff5cfeb7de84315
Antonio Correa <oi@antoniocorrea.com.br>
2026-03-04 19:25:25 -0300

    Utilizando shadcn para alguns componentes de animação dentro do layout

--- src/components/Fonts/Titleh1.jsx
+++ src/components/Fonts/Titleh1.jsx
@@ -1,10 +1,24 @@
 import React, { Children } from 'react'
+import SplitText from "../SplitText";
+
 
 const Titleh1 = ( { children } ) => {
   return (
-    <h1 className='font-display text-grayish-10 text-3xl lg:text-5xl font-bold mb-6'>
-      { children }
-    </h1>
+    <SplitText
+      text={ children }
+      className="font-display text-grayish-10 text-3xl lg:text-5xl font-bold mb-6"
+      delay={50}
+      duration={1.25}
+      ease="power3.out"
+      splitType="lines"
+      from={{ opacity: 0, y: 40 }}
+      to={{ opacity: 1, y: 0 }}
+      threshold={0.1}
+      rootMargin="-100px"
+      textAlign="inherit"
+      showCallback
+      tag='h1'
+    />
   )
 }
 
--- src/components/Fonts/Titleh2.jsx
+++ src/components/Fonts/Titleh2.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import SplitText from "../SplitText";
 
 const Titleh2 = ( { children } ) => {
   return (
-    <h2 className='font-display text-[1.5rem] text-grayish-10 mb-6 font-bold lg:text-[2rem]'>
-      { children }
-    </h2>
+    <SplitText
+      text={ children }
+      className="font-display text-[1.5rem] text-grayish-10 mb-6 font-bold lg:text-[2rem]"
+      delay={50}
+      duration={1.25}
+      ease="power3.out"
+      splitType="lines"
+      from={{ opacity: 0, y: 40 }}
+      to={{ opacity: 1, y: 0 }}
+      threshold={0.1}
+      rootMargin="-100px"
+      textAlign="inherit"
+      showCallback
+      tag='h2'
+    />
   )
 }
 
